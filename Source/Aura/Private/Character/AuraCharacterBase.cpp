28542393cb18b778de778bddf723c6b9e8b8cb34
irtoa <550584828@qq.com>
2025-11-16 16:33:05 +0800

    01_基本角色类及对应角色、动画蓝图配置

---
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -0,0 +1,23 @@
+
+
+
+#include "Character/AuraCharacterBase.h"
+
+
+AAuraCharacterBase::AAuraCharacterBase()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	Weapon = CreateDefaultSubobject<USkeletalMeshComponent>("Weapon"); // 创建武器网格组件
+	Weapon->SetupAttachment(GetMesh(), FName("WeaponHandSocket")); // 将武器附加到角色网格的武器手插槽
+	Weapon->SetCollisionEnabled(ECollisionEnabled::NoCollision); // 禁用碰撞
+}
+
+// Called when the game starts or when spawned
+void AAuraCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+
