3dd1510f10b76f21bf4914ca76c4c3e166555a89
irtoa <550584828@qq.com>
2026-02-09 22:56:43 +0800

    0915 添加了menu widget的controller类

---
+++ Source/Aura/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
@@ -0,0 +1,12 @@
+// 
+
+
+#include "UI/WidgetController/AttributeMenuWidgetController.h"
+
+void UAttributeMenuWidgetController::BroadcastInitialValues()
+{
+}
+
+void UAttributeMenuWidgetController::BindCallbacksToDependencies()
+{
+}
---
+++ Source/Aura/Public/UI/WidgetController/AttributeMenuWidgetController.h
@@ -0,0 +1,19 @@
+// 
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AuraWidgetController.h"
+#include "AttributeMenuWidgetController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UAttributeMenuWidgetController : public UAuraWidgetController
+{
+	GENERATED_BODY()
+public:
+	virtual void BroadcastInitialValues() override; // 初始化时广播属性值,更新UI
+	virtual void BindCallbacksToDependencies() override; // 绑定属性变化的回调函数
+};
